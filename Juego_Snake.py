325a1f701870d7631c90e77350ff6af7746e84cd
Regina González Q <a00827107@itesm.mx>
2020-10-28 00:03:09 -0600

    Cambio de color de la serpiente y la comida
    Le añadi la acción random, hice una lista con los colores que van a estar cambiando y establecí las variables que cambian los colores.

--- Juego_Snake.py
+++ Juego_Snake.py
@@ -9,6 +9,7 @@ import random
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+colores  = ["blue","orange","purple","pink","yellow"]
 
 #función que le da valores al vector aim que indica la dirección
 def change(x, y):
@@ -20,12 +21,13 @@ def change(x, y):
 def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
+
 #función que indica los límites de la ventana del juego y si la comida está dentro de la ventana.
 def inside(food):
     "Return True if food inside boundaries."
     return -200 < food.x < 190 and -200 < food.y < 190
 
-#funcino recursiva que controla el movimiento de la serpiente y la comida. Tambien incluye el proceso de la serpiente "comiendo".
+#función recursiva que controla el movimiento de la serpiente y la comida. Tambien incluye el proceso de la serpiente "comiendo".
 def move():
     "Move snake forward one segment."
     val=[-1,0,0,0,0,0,0,0,0,0,0,0,1]
@@ -38,7 +40,6 @@ def move():
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
 
-
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
         update()
@@ -50,15 +51,17 @@ def move():
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
+        
     else:
         snake.pop(0)
+    
 
     clear()
-
+    
     for body in snake:
-        square(body.x, body.y, 9, 'black')
-
-    square(food.x, food.y, 9, 'green')
+        square(body.x, body.y, 9, color_s)
+    
+    square(food.x, food.y, 9, color_f)
     update()
     ontimer(move, 100)
 
@@ -70,5 +73,11 @@ onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
+
+# variable para el color de la serpiente usando la lista de colores y la acción random
+color_s = random.choice(colores)
+# variable para el color de la comida usando la lista de colores y la acción random
+color_f = random.choice(colores)
+
 move()
 done()
