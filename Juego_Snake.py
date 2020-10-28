836a56c4816e87df7e7fe55da8c743bba02e6252
ErnestoIRS <A00827434@itesm.mx>
2020-10-28 11:33:03 -0600

    Comentarios finales y espaciado
    Se agregaron los comentarios faltantes y se separo por lineas de código

--- Juego_Snake.py
+++ Juego_Snake.py
@@ -10,7 +10,9 @@ import random
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
-colores  = ["blue","orange","purple","pink","yellow"]
+
+#Grupo de colores disponibles que pueden tomar la comida y la serpiente.
+colores  = ["blue","orange","purple","pink","yellow","cyan"]
 
 #Función que le da valores al vector aim que indica la dirección
 def change(x, y):
@@ -31,32 +33,40 @@ def inside(food):
 #Función recursiva que controla el movimiento de la serpiente y la comida. Tambien incluye el proceso de la serpiente "comiendo".
 def move():
     "Move snake forward one segment."
+    
+    #grupo de valores de movimiento que puede tener la comida. Se toma un nímero al azar para moverse en el eje x y el eje y.
     val=[-1,0,0,0,0,0,0,0,0,0,0,0,1]
-    head = snake[-1].copy()
     food.x= random.choice(val)*10+food.x
     food.y= random.choice(val)*10+food.y
+    
+    #Función que realiza el movimiento de la serpiente y hace que los cuadros que conforman su cuerpo sigan a "head"
+    head = snake[-1].copy()
     head.move(aim)
     
+    #Si la comida llega al limite del mapa, se mueve a un lugar aleatorio dentro der este.
     if not inside(food):
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
-
+        
+    #si la cabeza de la serpiente llega al limite del mapa, se acaba el juego.
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
         update()
         return
-
+    
     snake.append(head)
 
+    #Al tomar la comida, la serpiente crece y la comida cambia de lugar
     if head == food:
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
-        
+    
+    #Elimina el valor del vector que representa la cabeza de la serpiente para poder remplazarlo con el nuevo y simular el movimiento    
     else:
         snake.pop(0)
     
-
+    #Limpia los cuadros resaltados en la pantalla para solamente dejar a la serpiente y a la comida.
     clear()
     
     #Función para el cuerpo de la serpiente. El color random está definido por color_s
@@ -69,10 +79,13 @@ def move():
     ontimer(move, 100)
 
 #Main-code
+#Se crea la ventana del juego y se inicializa turtle
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
+
+#Teclas que controlan el movimiento de la serpiente
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
@@ -80,6 +93,8 @@ onkey(lambda: change(0, -10), 'Down')
 
 # variable para el color de la serpiente usando la lista de colores y la acción random
 color_s = random.choice(colores)
+colores.remove(color_s)
+
 # variable para el color de la comida usando la lista de colores y la acción random
 color_f = random.choice(colores)
 
